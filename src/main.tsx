7a66738c80a32efcdc3363ad267b21cd02ecca55
Emily Tsai <emilytsai@csu.fullerton.edu>
2026-04-18 22:28:40 -0700

    Authenticate ArcGIS globally and set arcGIS api key

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import esriConfig from '@arcgis/core/config'
 import './index.css'
 import App from './App.tsx'
+
+esriConfig.apiKey = import.meta.env.VITE_ARCGIS_KEY
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
