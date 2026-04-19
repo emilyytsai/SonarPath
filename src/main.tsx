1b4f4f91fa8da5ec85ee62597db7bb05d81133cf
emilyytsai <emilytsai@csu.fullerton.edu>
2026-04-19 02:05:38 -0700

    Fixed ArcGIS authentication + routing service

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import esriConfig from '@arcgis/core/config'
+import IdentityManager from '@arcgis/core/identity/IdentityManager'
 import './index.css'
 import App from './App.tsx'
 
 esriConfig.apiKey = import.meta.env.VITE_ARCGIS_KEY
+
+IdentityManager.registerToken({
+  server: 'https://route-api.arcgis.com',
+  token: import.meta.env.VITE_ARCGIS_KEY
+})
+
+IdentityManager.registerToken({
+  server: 'https://www.arcgis.com',
+  token: import.meta.env.VITE_ARCGIS_KEY
+})
+
+IdentityManager.registerToken({
+  server: 'https://services.arcgis.com',
+  token: import.meta.env.VITE_ARCGIS_KEY
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
